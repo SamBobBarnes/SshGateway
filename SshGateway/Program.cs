71e0147193a3d1c73c96e8d52fd39f3da16a4f56
sb17057 <sbarnes@vermeer.com>
2025-06-04 13:15:51 -0500

    basic run of ssh

--- SshGateway/Program.cs
+++ SshGateway/Program.cs
@@ -1,9 +1,13 @@
-﻿namespace SshGateway;
+﻿using System.Diagnostics;
+
+namespace SshGateway;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        var proc = Process.Start("ssh.exe", "hermes");
+        proc.WaitForExit();
     }
 }
